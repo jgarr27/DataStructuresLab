627f04f7b7a47bdb4989aacfbcacf39012bf104b
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2023-02-03 08:29:58 -0500

    submit_lab

---
+++ DataStructuresLab/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+    string word = "";
+    Console.WriteLine("Please enter a word you would like to reverse: ");
+    word = Console.ReadLine();
+    var stack = new Stack<char>();
+    foreach(char c in word)
+    {
+        stack.Push(c);
+    }
+    word = String.Empty;
+
+    while(stack.Count > 0)
+    {
+        word += stack.Pop();
+    }
+    Console.WriteLine(word);
+}
+
